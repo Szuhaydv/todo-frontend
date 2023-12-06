6840730e241b9463aa89db7d34b99dffd00dcaeb
Szuhay Dávid <szuhaydv@gmail.com>
2023-12-06 14:16:39 +0100

    v5.7

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,7 @@ import { Link, useNavigate } from 'react-router-dom'
         console.log(err)
         setLoading(false)
       })
-    })
+    }, [])
     const handleDelete = (id) => {
       setLoading(true)
           axios
